6da9e54ef517f78021d74aa1d6bbff9eb768b0aa
sleesm <test@test.com>
2025-06-11 08:57:47 +0900

    [feat] create a writing method to file in FileManager

--- ssd/src/main/java/FileManager.java
+++ ssd/src/main/java/FileManager.java
@@ -1,8 +1,63 @@
+import java.io.*;
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
 class FileManager {
+    public static final String BLANK_DATA = "0x00000000";
+    Map<Integer, String> hashmap = new HashMap<>();
+
     public void readFile(int index) {
     }
 
-    public void writeFile(int index, int value) {
+    public void writeFile(int index, String value) {
+        hashmap.put(index,value);
+
+        File file = getOrCreateFile();
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+            for (Entry<Integer, String> entry : hashmap.entrySet()) {
+                writer.write(entry.getKey() + " " + entry.getValue());
+                writer.newLine();
+            }
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    public String readNandFile(int index) {
+        File file = getOrCreateFile();
+        updateHashMapForNandFile(file);
+        return hashmap.getOrDefault(index, BLANK_DATA);
+    }
+
+    private void updateHashMapForNandFile(File file) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] tmp = line.split(" ");
+                hashmap.put(Integer.parseInt(tmp[0]), tmp[1]);
+            }
+        } catch (IOException e) {
+            System.out.println("파일 읽기 오류 발생");
+        }
+    }
+
+    private File getOrCreateFile() {
+        File file = new File("ssd_nand.txt");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                System.out.println("파일 생성 오료");
+                ;
+            }
+        }
+        return file;
+    }
 }
