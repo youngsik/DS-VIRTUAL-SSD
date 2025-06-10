1b5da17e852e272c859321d3faf408892dc460e4
hj <hj@example.com>
2025-06-10 16:21:28 +0900

    [feat] SSDManager 구조 설정

---
+++ ssd/src/main/java/SSDManager.java
@@ -0,0 +1,19 @@
+public class SSDManager {
+    private String action;
+    private int lbaLocation;
+    private String value;
+
+    public SSDManager(String action, int lbaLocation, String value) {
+        this.action = action;
+        this.lbaLocation = lbaLocation;
+        this.value = value;
+    }
+
+    public void fileRead() {
+
+    }
+
+    public void fileWrite() {
+
+    }
+}
