20e248e1e0b5fa3dc0203eea86540f06a3bcf924
hihi <you@example.com>
2025-06-11 13:40:11 +0900

    [test] FullReadCommand execute 실행 테스트 케이스 작성

---
+++ shell/src/test/java/com/samsung/testshell/commands/FullReadCommandTest.java
@@ -0,0 +1,29 @@
+package com.samsung.testshell.commands;
+
+import com.samsung.testshell.TestShellManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class FullReadCommandTest {
+
+    @Mock
+    TestShellManager testShellManager;
+
+    @InjectMocks
+    FullReadCommand fullReadCommand;
+
+    @DisplayName("execute 실행 테스트")
+    @Test
+    void executeTest() {
+        fullReadCommand.excute();
+        verify(testShellManager, times(1)).fullread();
+    }
+}
