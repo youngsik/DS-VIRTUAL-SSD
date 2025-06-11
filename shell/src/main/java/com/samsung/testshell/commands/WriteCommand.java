51c062f90adf665ecafd3c1d55730ee90c3a4744
hihi <you@example.com>
2025-06-11 14:08:06 +0900

    [test] WriteCommand execute 기능 개발

--- shell/src/main/java/com/samsung/testshell/commands/WriteCommand.java
+++ shell/src/main/java/com/samsung/testshell/commands/WriteCommand.java
@@ -1,11 +1,14 @@
 package com.samsung.testshell.commands;
 
 import com.samsung.testshell.ShellCommand;
+import com.samsung.testshell.TestShellManager;
 
 public class WriteCommand implements ShellCommand {
 
+    private TestShellManager testShellManager;
+
     @Override
     public void execute(Integer index, String value) {
-
+        testShellManager.write(index, value);
     }
 }
