95c0d582d9bd48f8606d9549ae036dacb27b14fa
hihi <you@example.com>
2025-06-11 14:06:14 +0900

    [test] ReadCommand 테스트 케이스 작성

---
+++ shell/src/test/java/com/samsung/testshell/commands/ReadCommandTest.java
@@ -0,0 +1,30 @@
+package com.samsung.testshell.commands;
+
+import com.samsung.testshell.TestShellManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class ReadCommandTest {
+
+    @Mock
+    TestShellManager testShellManager;
+
+    @InjectMocks
+    ReadCommand readCommand;
+
+    @DisplayName("execute 실행 테스트")
+    @Test
+    void executeTest() {
+        readCommand.execute(1, "0XFFFFFF");
+        verify(testShellManager, times(1)).read(1);
+    }
+}
