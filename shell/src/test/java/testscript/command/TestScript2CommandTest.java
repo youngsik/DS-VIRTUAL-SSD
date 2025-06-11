dccaa0a3ccd1da426f39bc69ed00a09b0c546f60
sihun <ohsh0731@gmail.com>
2025-06-11 13:14:08 +0900

    [test] TestScript2Command test

--- shell/src/test/java/testscript/command/TestScript2CommandTest.java
+++ shell/src/test/java/testscript/command/TestScript2CommandTest.java
@@ -1,4 +1,27 @@
 package testscript.command;
 
+import com.samsung.testscript.ScriptManager;
+import com.samsung.testscript.command.TestScript1Command;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class TestScript2CommandTest {
+    @Mock
+    ScriptManager scriptManager;
+    @InjectMocks
+    TestScript2Command testScript2Command;
+
+    @Test
+    void callTestScript2Command(){
+        testScript2Command.execute();
+
+        verify(scriptManager).testScript2();
+    }
+
 }
