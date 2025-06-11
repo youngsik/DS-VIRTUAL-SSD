3a58c38ae4ad5f753728cd3f15629cf94e7644d5
sihun <ohsh0731@gmail.com>
2025-06-11 09:49:37 +0900

    [refactor] ScriptManager

--- shell/src/main/java/testscript/ScriptManager.java
+++ shell/src/main/java/testscript/ScriptManager.java
@@ -3,6 +3,8 @@ package testscript;
 import main.SsdApplication;
 
 public class ScriptManager {
+    private static final String READ_PREFIX_COMMAND = "R ";
+    private static final String WRITE_PREFIX_COMMAND = "W ";
     private final SsdApplication ssdApplication;
 
     public ScriptManager(SsdApplication ssdApplication) {
@@ -10,17 +12,17 @@ public class ScriptManager {
     }
 
     public String read(Integer lba){
-        String command = "R " + lba;
+        String command = READ_PREFIX_COMMAND + lba;
         return ssdApplication.execute(command);
     }
 
     public void write(Integer lba, String value){
-        String command = "w " + lba + " " + value;
+        String command = WRITE_PREFIX_COMMAND + lba + " " + value;
         ssdApplication.execute(command);
     }
 
     public boolean readAndCompare(Integer lba, String compareValue){
-        String command = "R " + lba;
+        String command = READ_PREFIX_COMMAND + lba;
         String readValue = ssdApplication.execute(command);
         return compareValue.equals(readValue);
     }
--- shell/src/test/java/testscript/ScriptManagerTest.java
+++ shell/src/test/java/testscript/ScriptManagerTest.java
@@ -2,27 +2,31 @@ package testscript;
 
 import main.SsdApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class ScriptManagerTest {
+    public static final String DUMMY_VALUE = "0xAAAABBBB";
     @Mock
     SsdApplication ssdApplication;
+    @InjectMocks
+    ScriptManager scriptManager;
 
     @Test
     void createSscScriptManager(){
-        ScriptManager scriptManager = new ScriptManager(ssdApplication);
-
         assertNotNull(scriptManager);
     }
 
     @Test
     void callSsdApplicationReadTest(){
-        String expected = "0xAAAABBBB";
-        ScriptManager scriptManager = new ScriptManager(ssdApplication);
+        String expected = DUMMY_VALUE;
         when(ssdApplication.execute("R 1")).thenReturn(expected);
 
         String result = scriptManager.read(1);
@@ -33,20 +37,16 @@ public class ScriptManagerTest {
 
     @Test
     void callSsdApplicationWriteTest(){
-        ScriptManager scriptManager = new ScriptManager(ssdApplication);
-
-        scriptManager.write(1, "0xAAAABBBB");
+        scriptManager.write(1, DUMMY_VALUE);
 
         verify(ssdApplication).execute("W 1 0xAAAABBBB");
     }
 
     @Test
     void callSsdApplicationReadAndCompareTest(){
-        String expected = "0xAAAABBBB";
-        ScriptManager scriptManager = new ScriptManager(ssdApplication);
-        when(ssdApplication.execute("R 1")).thenReturn(expected);
+        when(ssdApplication.execute("R 1")).thenReturn(DUMMY_VALUE);
 
-        boolean result = scriptManager.readAndCompare(1, "0xAAAABBBB");
+        boolean result = scriptManager.readAndCompare(1, DUMMY_VALUE);
 
         verify(ssdApplication).execute("R 1");
         assertTrue(result);
