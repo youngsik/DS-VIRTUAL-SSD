7b1b70e53da802d79601141632a07fe7748f8e38
sleesm <test@test.com>
2025-06-11 12:46:29 +0900

    [refact] create test file's resetting method in FileManagerTest.java

--- ssd/src/test/java/FileManagerTest.java
+++ ssd/src/test/java/FileManagerTest.java
@@ -1,52 +1,65 @@
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Collections;
+import java.io.*;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class FileManagerTest {
 
+    public static final String TEST_ERROR_MESSAGE = "테스트에서 ssd_output.txt 파일 읽기 오류 발생";
+    public static final String TEST_SSD_OUTPUT_TXT_FILE_NAME = "src/test/resources/test_ssd_output.txt";
     public static final String CORRECT_VALUE = "0x1298CDEF";
     public static final int INDEX = 1;
 
     @InjectMocks
     private FileManager fileManager;
 
+    @BeforeEach
+    void setUp() {
+        resetTestOutputFile();
+    }
+
     @Test
     void write_success() {
         fileManager.writeFile(INDEX, CORRECT_VALUE);
 
-        assertByReadingSpecificIndexFile(CORRECT_VALUE);
+        assertByReadingSpecificIndexFile(INDEX, CORRECT_VALUE);
     }
 
     @Test
     void read_success() {
-        assertByReadingSpecificIndexFile(CORRECT_VALUE);
+        assertByReadingSpecificIndexFile(INDEX, CORRECT_VALUE);
     }
 
-    private void assertByReadingSpecificIndexFile(String expect) {
-        fileManager.readFile(INDEX);
+    private void resetTestOutputFile(){
+        try {
+            File file = new File(TEST_SSD_OUTPUT_TXT_FILE_NAME);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(CORRECT_VALUE);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            System.out.println(TEST_ERROR_MESSAGE);
+        }
+    }
+
+    private void assertByReadingSpecificIndexFile(int index, String expect) {
+        fileManager.readFile(index);
 
         try {
-            File file = new File("src/test/resources/test_ssd_output.txt");
+            File file = new File(TEST_SSD_OUTPUT_TXT_FILE_NAME);
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
             String ret = null;
             while ((ret = bufferedReader.readLine()) != null) {
                 assertThat(ret).isEqualTo(expect);
             }
         } catch (IOException e) {
-            System.out.println("테스트에서 ssd_output.txt 파일 읽기 오류 발생");
+            System.out.println(TEST_ERROR_MESSAGE);
         }
     }
 }
