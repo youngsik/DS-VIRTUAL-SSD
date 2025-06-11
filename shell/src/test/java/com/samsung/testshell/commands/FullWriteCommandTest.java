f068b65a1d720c983cabfdc4325bcfc99aa8e2b4
hihi <you@example.com>
2025-06-11 13:42:48 +0900

    [test] FullWriteCommand execute 실행 테스트 케이스 작성

---
+++ shell/src/test/java/com/samsung/testshell/commands/FullWriteCommandTest.java
@@ -0,0 +1,30 @@
+package com.samsung.testshell.commands;
+
+import com.samsung.testshell.TestShellManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class FullWriteCommandTest {
+
+    @Mock
+    TestShellManager testShellManager;
+
+    @InjectMocks
+    FullWriteCommand fullWriteCommand;
+
+    @DisplayName("execute 실행 테스트")
+    @Test
+    void excuteTest() {
+        fullWriteCommand.excute();
+        verify(testShellManager, times(1)).fullwrite();
+    }
+}
