6fb65fcfe39447ebd3450a9630a020676d7f78d6
hihi <you@example.com>
2025-06-11 16:23:38 +0900

    [refactor] ShellCommandInvoker 테스트 케이스 전역 상수화 리팩토링

--- shell/src/test/java/com/samsung/testshell/ShellCommandInvokerTest.java
+++ shell/src/test/java/com/samsung/testshell/ShellCommandInvokerTest.java
@@ -1,30 +1,31 @@
 package com.samsung.testshell;
 
-import com.samsung.testshell.commands.WriteCommand;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ShellCommandInvokerTest {
 
+    public static final String VALID_COMMAND_NAME = "write";
+    public static final String INVALID_COMMAND_NAME = "abcde";
+    public static final Integer INDEX = 1;
+    public static final String VALUE = "0XFFFFFF";
+    public static final ShellCommand EMPTY_COMMAND = null;
+
     @Mock
     private Map<String, ShellCommand> mockCommandMap;
 
     @Mock
-    private ShellCommand shellCommand;
+    private ShellCommand mockShellCommand;
 
     @InjectMocks
     private ShellCommandInvoker invoker;
@@ -32,36 +33,23 @@ class ShellCommandInvokerTest {
     @DisplayName("ShellCommand 객체 등록 실행 테스트")
     @Test
     void registerTest() {
-        String commandName = "write";
-
-        invoker.register(commandName, shellCommand);
-
-        verify(mockCommandMap, times(1)).put(commandName, shellCommand);
+        invoker.register(VALID_COMMAND_NAME, mockShellCommand);
+        verify(mockCommandMap, times(1)).put(VALID_COMMAND_NAME, mockShellCommand);
     }
 
     @DisplayName("ShellCommand exectue 메서드 실행 pass 테스트")
     @Test
     void execute_pass_test() {
-        String commandName = "write";
-        Integer index = 1;
-        String value = "0XFFFFFF";
+        doReturn(mockShellCommand).when(mockCommandMap).get(VALID_COMMAND_NAME);
 
-        doReturn(shellCommand).when(mockCommandMap).get(commandName);
-
-        invoker.execute(commandName, index, value);
-
-        verify(shellCommand, times(1)).execute(index, value);
+        invoker.execute(VALID_COMMAND_NAME, INDEX, VALUE);
+        verify(mockShellCommand, times(1)).execute(INDEX, VALUE);
     }
 
     @DisplayName("ShellCommand exectue 메서드 실행 예외 테스트")
     @Test
     void execute_exception_test() {
-        String invalidCommandName = "abcde";
-        Integer index = 1;
-        String value = "0XFFFFFF";
-
-        doReturn(null).when(mockCommandMap).get(invalidCommandName);
-
-        assertThrows(RuntimeException.class, () -> invoker.execute(invalidCommandName, index, value));
+        doReturn(EMPTY_COMMAND).when(mockCommandMap).get(INVALID_COMMAND_NAME);
+        assertThrows(RuntimeException.class, () -> invoker.execute(INVALID_COMMAND_NAME, INDEX, VALUE));
     }
 }
