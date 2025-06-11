6e7b169891287e0e05245535121f16d4b67a27f7
hihi <you@example.com>
2025-06-11 13:40:41 +0900

    [feat] FullReadCommand execute 기능 개발

--- shell/src/main/java/com/samsung/testshell/commands/FullReadCommand.java
+++ shell/src/main/java/com/samsung/testshell/commands/FullReadCommand.java
@@ -1,11 +1,18 @@
 package com.samsung.testshell.commands;
 
 import com.samsung.testshell.ShellCommand;
+import com.samsung.testshell.TestShellManager;
 
 public class FullReadCommand implements ShellCommand {
 
+    private TestShellManager testShellManager;
+
+    public FullReadCommand(TestShellManager testShellManager) {
+        this.testShellManager = testShellManager;
+    }
+
     @Override
     public void excute() {
-
+        testShellManager.fullread();
     }
 }
