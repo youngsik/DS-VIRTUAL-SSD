6872e86e321f7e488f7bd9f58964512513ea8053
hihi <you@example.com>
2025-06-11 13:43:03 +0900

    [feat] FullWriteCommand execute 기능 개발

--- shell/src/main/java/com/samsung/testshell/commands/FullWriteCommand.java
+++ shell/src/main/java/com/samsung/testshell/commands/FullWriteCommand.java
@@ -1,11 +1,18 @@
 package com.samsung.testshell.commands;
 
 import com.samsung.testshell.ShellCommand;
+import com.samsung.testshell.TestShellManager;
 
 public class FullWriteCommand implements ShellCommand {
 
+    private TestShellManager testShellManager;
+
+    public FullWriteCommand(TestShellManager testShellManager) {
+        this.testShellManager = testShellManager;
+    }
+
     @Override
     public void excute() {
-
+        testShellManager.fullwrite();
     }
 }
