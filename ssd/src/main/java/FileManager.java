e711ee5771668e1709a409308e3bdb57337e40d9
sleesm <test@test.com>
2025-06-11 09:47:37 +0900

    [chore] change a method line's order for readability

--- ssd/src/main/java/FileManager.java
+++ ssd/src/main/java/FileManager.java
@@ -10,10 +10,41 @@ class FileManager {
 
     private Map<Integer, String> hashmap = new HashMap<>();
 
+    private File getOrCreateFile(String fileName) {
+        File file = new File(fileName);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                System.out.println("파일 생성 오료");
+            }
+        }
+        return file;
+    }
+
     public void readFile(int index) {
         String result = readNandFile(index);
         File file = getOrCreateFile(SSD_OUTPUT_FILE_NAME);
         writeOnOutputFile(file, result);
+    }
+
+    public String readNandFile(int index) {
+        File file = getOrCreateFile(SSD_NAND_FILE_NAME);
+        updateHashMapForNandFile(file);
+        return hashmap.getOrDefault(index, BLANK_DATA);
+    }
+
+    private void updateHashMapForNandFile(File file) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] tmp = line.split(" ");
+                hashmap.put(Integer.parseInt(tmp[0]), tmp[1]);
+            }
+        } catch (IOException e) {
+            System.out.println("파일 읽기 오류 발생");
+        }
     }
 
     private void writeOnOutputFile(File file, String result) {
@@ -45,37 +76,6 @@ class FileManager {
             writer.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    public String readNandFile(int index) {
-        File file = getOrCreateFile(SSD_NAND_FILE_NAME);
-        updateHashMapForNandFile(file);
-        return hashmap.getOrDefault(index, BLANK_DATA);
-    }
-
-    private File getOrCreateFile(String fileName) {
-        File file = new File(fileName);
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                System.out.println("파일 생성 오료");
-            }
-        }
-        return file;
-    }
-
-    private void updateHashMapForNandFile(File file) {
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            String line = null;
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] tmp = line.split(" ");
-                hashmap.put(Integer.parseInt(tmp[0]), tmp[1]);
-            }
-        } catch (IOException e) {
-            System.out.println("파일 읽기 오류 발생");
         }
     }
 }
