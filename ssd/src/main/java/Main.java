f0b05aaff331967fae49a1afd724e4612527de43
sequence-and-query <lifeisvector@gmail.com>
2025-06-11 12:58:54 +0900

    [FEATURE] ADD MAIN

--- ssd/src/main/java/Main.java
+++ ssd/src/main/java/Main.java
@@ -2,4 +2,12 @@ class Main {
     public void print(){
         System.out.println("print SSD");
     }
+
+    public static void main(String[] args) {
+        SSDManager ssdManager = new SSDManager("W", 0, "0x00000002");
+        ssdManager.cmdExecute();
+
+        ssdManager = new SSDManager("W", 0, "0x00000001");
+        ssdManager.cmdExecute();
+    }
 }
