54a8acde6894d2949c8203e484a29a8b592d49ae
zilch369 <jk369.park@gmail.com>
2025-06-11 09:45:41 +0900

    [feat] ssdMain_parsing

--- ssd/src/main/java/Main.java
+++ ssd/src/main/java/Main.java
@@ -2,4 +2,14 @@ class Main {
     public void print(){
         System.out.println("print SSD");
     }
+
+    public String command;
+    public int lbaLocation;
+    public String value;
+
+    public void parsing(String[] args) {
+        command = args[0];
+        lbaLocation = Integer.parseInt(args[1]);
+        value = args[2];
+    }
 }
