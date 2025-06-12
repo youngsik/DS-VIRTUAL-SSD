9d4a512c844fc42a16cfacda9b2f716d19ff2027
hj <hj@example.com>
2025-06-12 10:49:17 +0900

    [refactor] 변수명 정리

--- ssd/src/main/java/com/samsung/SSDManager.java
+++ ssd/src/main/java/com/samsung/SSDManager.java
@@ -2,29 +2,31 @@ package com.samsung;
 
 class SSDManager {
     public static final String ERASE_VALUE = "0x00000000";
+    public static final int MAX_LBA = 99;
     private String command = "";
-    private int LBA = -1;
+    private int lba = -1;
     private String value = "";
     private FileManager fileManager;
 
-    public SSDManager(String command, int LBA, String value) {
+    public SSDManager(String command, int lba, String value) {
         this.command = command;
-        this.LBA = LBA;
+        this.lba = lba;
         this.value = value;
         this.fileManager = new FileManager();
     }
 
     public void cmdExecute() {
         if (command.equals("ERROR")) fileErrorOutput();
-        else if (command.equals("R")) fileRead(LBA);
-        else if (command.equals("W")) fileWrite(LBA, value);
-        else if (command.equals("E")) fileErase(LBA, Integer.parseInt(value));
+        else if (command.equals("R")) fileRead(lba);
+        else if (command.equals("W")) fileWrite(lba, value);
+        else if (command.equals("E")) fileErase(lba, Integer.parseInt(value));
     }
 
-    private void fileErase(int lba, int size) {
-        for (int i=lba; i<lba+size; i++) {
-            if(i > 99) break;
-            fileManager.writeFile(i, ERASE_VALUE);
+    private void fileErase(int startLba, int size) {
+        int endLba = startLba + size;
+        for (int currentLba = startLba; currentLba < endLba; currentLba++) {
+            if(currentLba > MAX_LBA) break;
+            fileManager.writeFile(currentLba, ERASE_VALUE);
         }
     }
 
@@ -32,11 +34,11 @@ class SSDManager {
         fileManager.writeOnOutputFile(value);
     }
 
-    public void fileRead(int lbaLocation) {
-        fileManager.readFile(lbaLocation);
+    public void fileRead(int lba) {
+        fileManager.readFile(lba);
     }
 
-    public void fileWrite(int lbaLocation, String value) {
-        fileManager.writeFile(lbaLocation, value);
+    public void fileWrite(int lba, String value) {
+        fileManager.writeFile(lba, value);
     }
 }
