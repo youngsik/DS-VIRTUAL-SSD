0edac97ab1b7c9a165be0955733b4396d4c34128
sleesm <sleepingsm813@gmail.com>
2025-06-12 13:24:56 +0900

    [refact] extract method for getting value from file

--- ssd/src/main/java/com/samsung/FileManager.java
+++ ssd/src/main/java/com/samsung/FileManager.java
@@ -17,15 +17,22 @@ public class FileManager {
     }
 
     public void readFile(int index) {
+        String result = getValueFromFile(index);
+        writeOnOutputFile(result);
+    }
+
+    public String getValueFromFile(int index) {
+        String result = null;
         try {
             RandomAccessFile file = new RandomAccessFile(SSD_NAND_FILE_NAME, "r");
             file.seek(index * OFFSET);
             byte[] buf = new byte[10];
             file.read(buf);
-            writeOnOutputFile(new String(buf));
+            result = new String(buf);
         } catch (IOException e) {
 
         }
+        return result;
     }
 
     public void writeFile(int index, String value) {
