8a151b84fc8bf451d7ef15512b6461f8401502a7
hj <hj@example.com>
2025-06-12 11:09:45 +0900

    [refactor] command 명령어 상수로 추출 및 상수들은 별도의 클래스로 추출, fileRead/Write함수 제외함에 따라 중복되는 TestCase 제거

--- ssd/src/main/java/com/samsung/SSDManager.java
+++ ssd/src/main/java/com/samsung/SSDManager.java
@@ -1,8 +1,6 @@
 package com.samsung;
 
 class SSDManager {
-    public static final String ERASE_VALUE = "0x00000000";
-    public static final int MAX_LBA = 99;
     private String command = "";
     private int lba = -1;
     private String value = "";
@@ -16,29 +14,21 @@ class SSDManager {
     }
 
     public void cmdExecute() {
-        if (command.equals("ERROR")) fileErrorOutput();
-        else if (command.equals("R")) fileRead(lba);
-        else if (command.equals("W")) fileWrite(lba, value);
-        else if (command.equals("E")) fileErase(lba, Integer.parseInt(value));
+        if (command.equals(SSDConstant.COMMAND_ERROR)) fileErrorOutput();
+        else if (command.equals(SSDConstant.COMMAND_READ)) fileManager.readFile(lba);
+        else if (command.equals(SSDConstant.COMMAND_WRITE)) fileManager.writeFile(lba, value);
+        else if (command.equals(SSDConstant.COMMAND_ERASE)) fileErase(lba, Integer.parseInt(value));
     }
 
     private void fileErase(int startLba, int size) {
         int endLba = startLba + size;
         for (int currentLba = startLba; currentLba < endLba; currentLba++) {
-            if(currentLba > MAX_LBA) break;
-            fileManager.writeFile(currentLba, ERASE_VALUE);
+            if(currentLba > SSDConstant.MAX_LBA) break;
+            fileManager.writeFile(currentLba, SSDConstant.ERASE_VALUE);
         }
     }
 
     private void fileErrorOutput() {
         fileManager.writeOnOutputFile(value);
-    }
-
-    public void fileRead(int lba) {
-        fileManager.readFile(lba);
-    }
-
-    public void fileWrite(int lba, String value) {
-        fileManager.writeFile(lba, value);
     }
 }
--- ssd/src/test/java/com/samsung/SSDManagerTest.java
+++ ssd/src/test/java/com/samsung/SSDManagerTest.java
@@ -29,37 +29,23 @@ public class SSDManagerTest {
     @Test
     @DisplayName("읽기 테스트")
     public void readTest() {
-        readSsdManager.fileRead(LBA);
+        readSsdManager.cmdExecute();
         verify(fileManager, times(1)).readFile(LBA);
     }
 
     @Test
     @DisplayName("쓰기 테스트")
     public void writeTest() {
-        writeSsdManager.fileWrite(LBA, WRITE_VALUE);
+        writeSsdManager.cmdExecute();
         verify(fileManager, times(1)).writeFile(LBA, WRITE_VALUE);
     }
 
     @Test
     @DisplayName("읽기 2번 테스트")
     public void readTwiceTest() {
-        readSsdManager.fileRead(LBA);
-        readSsdManager.fileRead(LBA);
-        verify(fileManager, times(2)).readFile(LBA);
-    }
-
-    @Test
-    @DisplayName("읽기 명령어 테스트(성공)")
-    void cmdExecuteReadPass() {
         readSsdManager.cmdExecute();
-        verify(fileManager, times(1)).readFile(LBA);
-    }
-
-    @Test
-    @DisplayName("쓰기 명령어 테스트(성공)")
-    void cmdExecuteWritePass() {
-        writeSsdManager.cmdExecute();
-        verify(fileManager, times(1)).writeFile(LBA, WRITE_VALUE);
+        readSsdManager.cmdExecute();
+        verify(fileManager, times(2)).readFile(LBA);
     }
 
     @Test
