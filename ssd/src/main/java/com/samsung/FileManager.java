ff3282708bf2ed1afca5910abb0650578536c5f1
sleesm <sleepingsm813@gmail.com>
2025-06-12 12:53:18 +0900

    [refact] use RandomAccessFile class instead of BufferedReader/Writer

--- ssd/src/main/java/com/samsung/FileManager.java
+++ ssd/src/main/java/com/samsung/FileManager.java
@@ -1,34 +1,60 @@
 package com.samsung;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 import java.util.Map.Entry;
+import java.util.stream.Stream;
 
 public class FileManager {
     public static final String BLANK_DATA = "0x00000000";
     public static final String SSD_NAND_FILE_NAME = "ssd_nand.txt";
     public static final String SSD_OUTPUT_FILE_NAME = "ssd_output.txt";
+    public static final int OFFSET = 11;
 
-    private Map<Integer, String> hashmap = new HashMap<>();
+    public FileManager() {
+        resetNandFile();
+    }
 
+    /// ssd_nand.txt 예시
+    /// 0x12345678
+    /// 0xABCDEFGH
+    /// 0x1298CDEF
+    /// 0xABCDEFGH
     public void readFile(int index) {
-        settingHashMapFromNandFile();
-        String result = getValue(index);
-        writeOnOutputFile(result);
+        try {
+            RandomAccessFile file = new RandomAccessFile(SSD_NAND_FILE_NAME, "r");
+            file.seek(index * OFFSET);
+            byte[] buf = new byte[10];
+            file.read(buf);
+            writeOnOutputFile(new String(buf));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void writeFile(int index, String value) {
-        settingHashMapFromNandFile();
-        hashmap.put(index,value);
-        writeOnNandFile();
+        try {
+            RandomAccessFile file = new RandomAccessFile(SSD_NAND_FILE_NAME, "rw");
+            file.seek(index * OFFSET);
+            file.writeBytes(value);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private void settingHashMapFromNandFile() {
-        List<String> data = getDataFromNandFile();
-        updateHashMap(data);
+    public void writeOnOutputFile(String result) {
+        File file = getOrCreateFile(SSD_OUTPUT_FILE_NAME);
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(result);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+
+        }
     }
 
     private File getOrCreateFile(String fileName) {
@@ -43,52 +69,14 @@ public class FileManager {
         return file;
     }
 
-    private List<String> getDataFromNandFile() {
-        File file = getOrCreateFile(SSD_NAND_FILE_NAME);
-        List<String> data = new ArrayList<>();
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            String line = null;
-            while ((line = bufferedReader.readLine()) != null) {
-                data.add(line);
-            }
-        } catch (IOException e) {
-
-        }
-        return data;
-    }
-
-    private String getValue(int index) {
-        return hashmap.getOrDefault(index, BLANK_DATA);
-    }
-
-    private void updateHashMap(List<String> data) {
-        for(String d : data){
-            String[] tmp = d.split(" ");
-            hashmap.put(Integer.parseInt(tmp[0]), tmp[1]);
-        }
-    }
-
-    private void writeOnNandFile() {
+    private void resetNandFile() {
         File file = getOrCreateFile(SSD_NAND_FILE_NAME);
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-            for (Entry<Integer, String> entry : hashmap.entrySet()) {
-                writer.write(entry.getKey() + " " + entry.getValue());
-                writer.newLine();
+            for (int i = 0; i < 98; i++) {
+                writer.write(BLANK_DATA + "\n");
             }
-            writer.flush();
-            writer.close();
-        } catch (IOException e) {
-
-        }
-    }
-
-    public void writeOnOutputFile(String result) {
-        File file = getOrCreateFile(SSD_OUTPUT_FILE_NAME);
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-            writer.write(result);
+            writer.write(BLANK_DATA);
             writer.flush();
             writer.close();
         } catch (IOException e) {
