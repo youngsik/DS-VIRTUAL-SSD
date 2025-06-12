93f0ecca2dc096fbb21ca8bc24ad69e4c960cf5a
sequence-and-query <lifeisvector@gmail.com>
2025-06-12 13:07:42 +0900

    [FEATURE] Add EraseCommand TestCode

--- shell/src/test/java/com/samsung/command/testshell/EraseRangeCommandTest.java
+++ shell/src/test/java/com/samsung/command/testshell/EraseRangeCommandTest.java
@@ -1,7 +1,46 @@
 package com.samsung.command.testshell;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class EraseRangeCommandTest {
+    @Mock
+    TestShellManager testShellManager;
 
+    @InjectMocks
+    EraseRangeCommand eraseRangeCommand;
+
+    @Test
+    public void validLBA() {
+        Random random = new Random();
+
+        int begin = random.nextInt(100);
+        int end = random.nextInt(100);
+
+        String[] cmdArgs = new String[]{"erase_range", String.valueOf(begin), String.valueOf(end)};
+
+        eraseRangeCommand.execute(cmdArgs);
+        Mockito.verify(testShellManager, Mockito.times(1)).eraseRange(Mockito.anyInt(), Mockito.anyInt());
+    }
+
+    @Test
+    public void invalidLBA() {
+        Random random = new Random();
+
+        int begin = random.nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE);
+        int end = random.nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE);
+
+        String[] cmdArgs = new String[]{"erase_range", String.valueOf(begin), String.valueOf(end)};
+
+        assertThrows(RuntimeException.class, () -> eraseRangeCommand.execute(cmdArgs));
+    }
 }
