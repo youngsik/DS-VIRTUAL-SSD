4eff47388b30078aa4e146ec53d359d19f6f601b
hihi <you@example.com>
2025-06-13 11:11:25 +0900

    [refactor] EraseRangeCommand 리팩토링 (private 메서드로 모듈화)

--- shell/src/main/java/com/samsung/command/testshell/EraseRangeCommand.java
+++ shell/src/main/java/com/samsung/command/testshell/EraseRangeCommand.java
@@ -4,6 +4,7 @@ import com.samsung.validator.ArgumentsValidator;
 import com.samsung.validator.CommandValidator;
 
 public class EraseRangeCommand implements Command {
+
     private final TestShellManager testShellManager;
 
     public EraseRangeCommand(TestShellManager testShellManager) {
@@ -13,20 +14,30 @@ public class EraseRangeCommand implements Command {
     @Override
     public void execute(String[] cmdArgs) {
         ArgumentsValidator.validateThreeArgs(cmdArgs);
-
-        int beginLBA;
-        int endLBA;
-
-        try{
-            beginLBA = Integer.parseInt(cmdArgs[1]);
-            endLBA = Integer.parseInt(cmdArgs[2]);
-        } catch(NumberFormatException e) {
-            throw new RuntimeException("INVALID COMMAND PARAMETER");
-        }
-
-        CommandValidator.validateLbaRange(beginLBA);
-        CommandValidator.validateLbaRange(endLBA);
-
-        testShellManager.eraseRange(beginLBA, endLBA);
+        testShellManager.eraseRange(
+                extractValidatedBeginLba(cmdArgs),
+                extractValidatedEndLba(cmdArgs));
     }
+
+    private Integer extractValidatedBeginLba(String[] cmdArgs) {
+        Integer beginLBA = toInt(cmdArgs[1]);
+        CommandValidator.validateLbaRange(beginLBA);
+        return beginLBA;
+    }
+
+    private Integer extractValidatedEndLba(String[] cmdArgs) {
+        Integer endLba = toInt(cmdArgs[2]);
+        CommandValidator.validateLbaRange(endLba);
+        return endLba;
+    }
+
+
+    private Integer toInt(String arg) {
+        try{
+            return Integer.parseInt(arg);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("INVALID COMMAND");
+        }
+    }
+
 }
