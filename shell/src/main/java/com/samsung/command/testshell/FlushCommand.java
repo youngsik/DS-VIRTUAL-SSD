e81defa65b3e37dd02b67ddf35ebd5fead860dfd
hihi <you@example.com>
2025-06-13 10:21:32 +0900

    [refactor] FlushCommand 유효성 검증 메서드 사용하도록 수정

--- shell/src/main/java/com/samsung/command/testshell/FlushCommand.java
+++ shell/src/main/java/com/samsung/command/testshell/FlushCommand.java
@@ -1,8 +1,10 @@
 package com.samsung.command.testshell;
 
 import com.samsung.command.Command;
+import com.samsung.validator.ArgumentsValidator;
 
 public class FlushCommand implements Command {
+
     private final TestShellManager testShellManager;
 
     public FlushCommand(TestShellManager testShellManager) {
@@ -11,10 +13,7 @@ public class FlushCommand implements Command {
 
     @Override
     public void execute(String[] cmdArgs) {
-        if(cmdArgs.length != 1) {
-            throw new RuntimeException("FLUSH COMMAND ERROR");
-        }
-
+        ArgumentsValidator.validateOneArgs(cmdArgs);
         testShellManager.flush();
     }
 }
