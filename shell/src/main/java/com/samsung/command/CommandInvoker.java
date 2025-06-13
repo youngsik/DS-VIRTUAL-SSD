d5aa6033a59003325eff5e46457fabecbf0d155a
hihi <you@example.com>
2025-06-13 19:54:08 +0900

    [test] CommandRegistry 사용하도록 CommandInvoker 수정 및 테스트 케이스 리팩토링

--- shell/src/main/java/com/samsung/command/CommandInvoker.java
+++ shell/src/main/java/com/samsung/command/CommandInvoker.java
@@ -1,71 +1,26 @@
 package com.samsung.command;
 
-import com.samsung.file.FileManager;
-import com.samsung.command.testscript.*;
-import com.samsung.command.testshell.*;
-import com.samsung.file.JarExecutor;
-
-import java.util.Map;
-
 public class CommandInvoker {
 
-    private final Map<String, Command> commandMap;
+    private static final int COMMAND_NAME_INDEX = 0;
 
-    public CommandInvoker(Map<String, Command> commandMap) {
-        this.commandMap = commandMap;
-        initShellCommand();
-        initScriptCommand();
+    private final CommandRegistry registry;
+
+    public CommandInvoker(CommandRegistry registry) {
+        this.registry = registry;
     }
 
     public void execute(String[] cmdArgs) {
-        Command command = getCommand(cmdArgs[0]);
+        Command command = registry.getCommand(cmdArgs[COMMAND_NAME_INDEX]);
+        validateCommandNotNull(command);
+
         command.execute(cmdArgs);
     }
 
-    private Command getCommand(String commandName) {
-        Command command = commandMap.get(commandName);
-
+    private void validateCommandNotNull(Command command) {
         if (command == null) {
             throw new RuntimeException("INVALID COMMAND");
         }
-
-        return command;
     }
 
-    private void initShellCommand() {
-        TestShellManager testShellManager = new TestShellManager(new JarExecutor(), FileManager.getInstance());
-        register("write", new WriteCommand(testShellManager));
-        register("read", new ReadCommand(testShellManager));
-        register("erase", new EraseCommand(testShellManager));
-        register("erase_range", new EraseRangeCommand(testShellManager));
-        register("exit", new ExitCommand(testShellManager));
-        register("help", new HelpCommand(testShellManager));
-        register("fullwrite", new FullWriteCommand(testShellManager));
-        register("fullread", new FullReadCommand(testShellManager));
-        register("flush", new FlushCommand(testShellManager));
-    }
-
-    private void register(String commandName, Command command) {
-        commandMap.put(commandName, command);
-    }
-
-    private void initScriptCommand() {
-        ScriptManager scriptManager = new ScriptManager(FileManager.getInstance(), new JarExecutor(), RandomHex.getInstance());
-
-        String[] script1CommandNames = {"1_FullWriteAndReadCompare", "1_"};
-        String[] script2CommandNames = {"2_PartialLBAWrite", "2_"};
-        String[] script3CommandNames = {"3_WriteReadAging", "3_"};
-        String[] script4CommandNames = {"4_EraseAndWriteAging", "4_"};
-
-        register(script1CommandNames, new TestScript1Command(scriptManager));
-        register(script2CommandNames, new TestScript2Command(scriptManager));
-        register(script3CommandNames, new TestScript3Command(scriptManager));
-        register(script4CommandNames, new TestScript4Command(scriptManager));
-    }
-
-    private void register(String[] commandNames, Command command) {
-        for (String commandName : commandNames) {
-            commandMap.put(commandName, command);
-        }
-    }
 }
