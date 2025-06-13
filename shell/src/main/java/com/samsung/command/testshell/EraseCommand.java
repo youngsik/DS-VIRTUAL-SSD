33c096bb5faac6eaefd0416f74c7a393133f6fd9
hihi <you@example.com>
2025-06-13 11:11:19 +0900

    [refactor] EraseCommand 리팩토링 (private 메서드로 모듈화)

--- shell/src/main/java/com/samsung/command/testshell/EraseCommand.java
+++ shell/src/main/java/com/samsung/command/testshell/EraseCommand.java
@@ -5,6 +5,7 @@ import com.samsung.validator.ArgumentsValidator;
 import com.samsung.validator.CommandValidator;
 
 public class EraseCommand implements Command {
+
     private final TestShellManager testShellManager;
 
     public EraseCommand(TestShellManager testShellManager) {
@@ -14,19 +15,27 @@ public class EraseCommand implements Command {
     @Override
     public void execute(String[] cmdArgs) {
         ArgumentsValidator.validateThreeArgs(cmdArgs);
+        testShellManager.erase(
+                extractValidatedEraseLba(cmdArgs),
+                extractValidatedEraseSize(cmdArgs));
+    }
 
-        int eraseLBA;
-        int eraseSize;
-
-        try{
-            eraseLBA = Integer.parseInt(cmdArgs[1]);
-            eraseSize = Integer.parseInt(cmdArgs[2]);
-        }catch(NumberFormatException e) {
-            throw new RuntimeException("INVALID ERASE START LOCATION");
-        }
-
+    private Integer extractValidatedEraseLba(String[] cmdArgs) {
+        Integer eraseLBA = toInt(cmdArgs[1]);
         CommandValidator.validateLbaRange(eraseLBA);
+        return eraseLBA;
+    }
 
-        testShellManager.erase(eraseLBA, eraseSize);
+    private Integer extractValidatedEraseSize(String[] cmdArgs) {
+        Integer eraseSize = toInt(cmdArgs[2]);
+        return eraseSize;
+    }
+
+    private Integer toInt(String arg) {
+        try{
+            return Integer.parseInt(arg);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("INVALID COMMAND");
+        }
     }
 }
